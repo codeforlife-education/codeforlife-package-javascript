a487ba5a8c1781f3ed9b6b6284d7fedd574ae076
SKairinos <skairinos@gmail.com>
2026-07-06 14:22:44 +0000

    fix: permissions

--- src/api/tagTypes.ts
+++ src/api/tagTypes.ts
@@ -1,6 +1,6 @@
 const tagTypes = [
   // These are the tags for the common models used throughout our system.
-  // https://github.com/ocadotechnology/codeforlife-package-python/tree/main/codeforlife/user/models
+  // https://github.com/codeforlife-education/codeforlife-package-python/tree/main/codeforlife/user/models
   // NOTE: Don't use the "Teacher" and "Student" tags. Use "User" instead.
   "User",
   "School",
